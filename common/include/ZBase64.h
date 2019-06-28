c3143f6342df9b4dffa6fd7f490cf84131249600
jk983294 <jk983294@gmail.com>
2019-06-28 22:45:30 +0800

    init

---
+++ common/include/ZBase64.h
@@ -0,0 +1,9 @@
+#ifndef ZERG_BASE64_H
+#define ZERG_BASE64_H
+
+#include <string>
+
+std::string Base64Encode(std::string input);
+std::string Base64Decode(std::string const& s);
+
+#endif
---
+++ prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# backward.cpp config
+sudo apt-get install binutils-dev
+# g++/clang++ -lbfd -ldl
+# define BACKWARD_HAS_BFD 1
+
+# apt-get install libdw-dev
+# g++/clang++ -ldw ...
