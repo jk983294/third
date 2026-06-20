f86179d7eeb38893de9d692fb928ee7476274060
jk983294 <jk983294@gmail.com>
2026-06-20 16:32:07 +0800

    test util

--- common/include/test/TestHelper.h
+++ common/include/test/TestHelper.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <string>
+#include <vector>
 
 constexpr double epsilon = 1e-6;
 constexpr int default_least = 1;
@@ -23,4 +24,13 @@ bool FloatEqual(T1 a, T2 b) {
     } else {
         return false;
     }
+}
+
+template <typename T1, typename T2>
+bool FloatEqual(const std::vector<T1>& a, const std::vector<T2>& b) {
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); i++) {
+        if (!FloatEqual(a[i], b[i])) return false;
+    }
+    return true;
 }
